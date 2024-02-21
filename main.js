9b03f5870e2de1b4ae7345e3312d641d883e7608
Kamariddinov Orzubek <qorzubek394@gmail.com>
2024-02-21 11:42:53 -0500

    I added a block time indicator

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
+var wrongAttempts = 0;
+var blockedUntil = 0;
+
 function checkPassword() {
-    var password = document.getElementById('password').value;
-    // Bu erda siz kerakli parolni tekshirish shartlarini yozishingiz mumkin
-    if (password === 'Orzu') { // sizning_parolingiz o'rniga o'zingizning kerakli parolingizni yozing
-      window.location.href = 'site.html'; // Agar parol to'g'ri bo'lsa, foydalanuvchi sahifaga o'tkaziladi
-    } else {
-      document.getElementById('error-message').classList.remove('d-none'); // Agar parol noto'g'ri bo'lsa, xabar ko'rsatiladi
+    if (blockedUntil > Date.now()) { // Agar bloklangan muddat tugagan bo'lsa
+        var remainingTime = Math.ceil((blockedUntil - Date.now()) / 1000); // Qolgan vaqtni hisoblash (sekund)
+        alert('Kirish uchun hozircha bloklangan. Keyingi urinishga qolgan vaqt: ' + remainingTime + ' sekund');
+        return;
     }
-  }
+    
+    var password = document.getElementById('password').value;
+    if (password === 'Orzu') { 
+        window.location.href = 'site.html'; 
+    } else {
+        document.getElementById('error-message').style.display = 'block';
+        wrongAttempts++;
+        if (wrongAttempts >= 3) { // Agar 3 marta yoki undan ko'p urinish bo'lsa
+            blockedUntil = Date.now() + 5000; // 5 sekund mobaynida bloklangan
+            setTimeout(function() { // 5 daqiya o'tgachdan so'ng
+                wrongAttempts = 0; // Urinishlar sonini nolga o'zgartirish
+            }, 5000); // 5 sekund (5000 milliseconds)
+        }
+    }
+}
