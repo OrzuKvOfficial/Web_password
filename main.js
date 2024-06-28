ed4e4932d6f4ebb37599e3c69cc151e25a456933
Kamariddinov Orzubek <qorzubek394@gmail.com>
2024-06-27 23:57:42 -0400

    Web password

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ var wrongAttempts = 0;
 var blockedUntil = 0;
 
 function checkPassword() {
-    if (blockedUntil > Date.now()) { // Agar bloklangan muddat tugagan bo'lsa
-        var remainingTime = Math.ceil((blockedUntil - Date.now()) / 1000); // Qolgan vaqtni hisoblash (sekund)
+    if (blockedUntil > Date.now()) {
+        var remainingTime = Math.ceil((blockedUntil - Date.now()) / 1000);
         alert('Kirish uchun hozircha bloklangan. Keyingi urinishga qolgan vaqt: ' + remainingTime + ' sekund');
         return;
     }
@@ -14,11 +14,11 @@ function checkPassword() {
     } else {
         document.getElementById('error-message').style.display = 'block';
         wrongAttempts++;
-        if (wrongAttempts >= 3) { // Agar 3 marta yoki undan ko'p urinish bo'lsa
-            blockedUntil = Date.now() + 5000; // 5 sekund mobaynida bloklangan
-            setTimeout(function() { // 5 daqiya o'tgachdan so'ng
-                wrongAttempts = 0; // Urinishlar sonini nolga o'zgartirish
-            }, 5000); // 5 sekund (5000 milliseconds)
+        if (wrongAttempts >= 3) {
+            blockedUntil = Date.now() + 5000;
+            setTimeout(function() {
+                wrongAttempts = 0;
+            }, 5000);
         }
     }
 }
